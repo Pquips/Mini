850a6eb939b0e065d06199dfc8446d8eea0c53f5
Pquips <amirrose4@gmail.com>
2017-12-13 12:52:28 -0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,3 +1,8 @@
+window.onscroll = function() {scrollFunction()};
 
-
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 //document.getElementById("hiram").setAttribute("data-image-src", "images/LEDS1.png");
