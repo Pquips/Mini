9cca9d44d6a50dea2688d5cef27ed4b038d22f5a
Pquips <amirrose4@gmail.com>
2017-12-13 14:10:02 -0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,12 +1,3 @@
-window.onscroll = function() {scrollFunction()};
-
-// When the user clicks on the button, scroll to the top of the document
-function top() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
-
 function willyA(e){
 	document.getElementById("heaven").play();
 }
