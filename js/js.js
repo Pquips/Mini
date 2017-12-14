39b153b11c74592d1e2fec75fe35fada5834cd84
Pquips <amirrose4@gmail.com>
2017-12-13 20:19:01 -0800

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,28 +1,29 @@
 function willyA(e){
 	document.getElementById("heaven").play();
 }
+//When onclick="willyA" in HTML triggers id="heaven" which is connected to heaven.mp3 to play
 
 function willyD(e){
 	document.getElementById("hell").play();
 	
+//When onclick="willyD" in HTML triggers id="hell" which is connected to hell.mp3 to play
+	
 function atw(e){
 	document.getElementById("at").classList.toggle("angelteeb");
 }
+//When onclick="atw" in HTML triggers id="at" to toogle class="angelteeb"
 
 function dtw(e){
 	document.getElementById("dt").classList.toggle("demonteeb");
 }
+//When onclick="dtw" in HTML triggers id="dt" to toogle class="demonteeb"
 
 function asw(e){
 	document.getElementById("as").classList.toggle("angelsweaterb");
 }
-
+//When onclick="asw" in HTML triggers id="as" to toogle class="angelsweaterb"
+	
 function dsw(e){
 	document.getElementById("ds").classList.toggle("demonsweaterb");
 }
-
-function changePicture(e){
-	document.getElementById("vegeta").classList.toggle("power-up");
-}
-
-//document.getElementById("hiram").setAttribute("data-image-src", "images/LEDS1.png");
+//When onclick="dsw" in HTML triggers id="ds" to toogle class="demonsweaterb"
